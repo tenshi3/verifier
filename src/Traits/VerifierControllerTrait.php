4cc234f1dc9ef7f0533f5d6a1b25f26b3ff2da7c
Jon Massey <jon@lawyersonline.co.uk>
2015-05-21 11:51:56 +0100

    Initial controller mixin trait

---
+++ src/Traits/VerifierControllerTrait.php
@@ -0,0 +1,84 @@
+<?php namespace Coderjp\Verifier\Traits;
+
+/**
+ * This file is part of Verifier,
+ *
+ * Adds routes to the controller for verifying email addresses
+ *
+ * @license MIT
+ * @package Coderjp\Verifier
+ */
+
+use Auth;
+use Illuminate\Http\Request;
+
+trait VerifierControllerTrait
+{
+    /**
+     * @param Request $request
+     * @param string $code
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function verifyAttempt(Request $request, $code = null)
+    {
+        // If a code is specified, attempt to verify it
+        if(!empty($code)) {
+            $model = static::getModel();
+            $user = $model::verify($code);
+
+            // If verified, login the user TODO: Optional?
+            if ($user) {
+                Auth::login($user);
+                // TODO: include message confirming success
+                return redirect(property_exists($this, 'redirectAfterVerify') ? $this->redirectAfterVerify : '/');
+            }
+        }
+
+        return view(Config::get('verifier.verify_template'))->with('attempt', $code ? true : false);
+    }
+
+    /**
+     * Posted email address, attempts to resend verification message
+     *
+     * @param Request $request
+     * @return $this|\Illuminate\Http\RedirectResponse
+     */
+    public function resendVerification(Request $request)
+    {
+        // Validate email address
+        $this->validate($request, [
+            'email' => 'email|required',
+        ]);
+
+        // Retrieve user with that email address
+        $model = static::getModel();
+        $user = $model::where('email', $request->get('email'))->first();
+
+        // If the user is valid and not verified, send verification email
+        if($user && !$user->verified) {
+
+            $user->sendVerification();
+
+            // TODO: Attach sent message
+            return redirect()->back()
+                ->with([
+                    'message' => true,
+                ]);
+        } else {
+            return redirect()->back()
+                ->withErrors([
+                    'email' => 'Invalid email address',
+                ]);
+        }
+    }
+
+    /**
+     * Model used for login, typically \App\User
+     * @return string
+     */
+    public static function getModel()
+    {
+        return "\\App\\User";
+    }
+
+}
